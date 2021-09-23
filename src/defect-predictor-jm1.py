38483d1e5f81a30cbf12e258b8d6b312f82c183d
Carlos Cadel <carlos.cadel@sogeti.com>
2021-09-23 11:31:57 +0200

    Modified the dataset and did research about the different NB

--- src/defect-predictor-jm1.py
+++ src/defect-predictor-jm1.py
@@ -2,92 +2,55 @@
 # coding: utf-8
 
 # In[ ]:
-import os
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import confusion_matrix, plot_precision_recall_curve
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import ComplementNB
+from sklearn.preprocessing import KBinsDiscretizer
 
-
-useScaler = True
-verbose = 0
-
+# Import dataset
 df1 = pd.read_csv("datasets/jm1.csv")
 
-# xs = df1[['defects']]
-# ys = pd.Series(df1['New cases'])
-#
-# pd.to_numeric(ys)
-#
-# # let's visualize it
-# plt.xlabel('New recovered')
-# plt.ylabel('New cases')
-# plt.scatter(xs,ys)
-
 # Fix invalid values
-dropped_na_df1 = df1.dropna()
-dropped_duplicates_df1 = dropped_na_df1.drop_duplicates()
+df1.dropna(inplace=True)
+df1.drop_duplicates(inplace=True)
 
-df1 = dropped_duplicates_df1
-
+# Print info
 df1.info()
-
-# Outliers
-plt.boxplot(df1)
-plt.show()
-
-# Heatmap
-plt.figure(figsize=(12, 10))
-cor = df1.corr()
-sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
-plt.show()
 
 # Split up the data set into the features and the labels.
 X = df1.drop('defects', axis=1)  # Remove the ___ label.
-y = df1['defects']  # Only take out the ___ label.
 
-# X = input
-# y = output (defects)
-
-# Optionally drop features to see how this influences the result.
-# X.drop('e', axis=1, inplace=True)
+# Remove outliers and the no-relation values
+X.drop('e', axis=1, inplace=True)
 X.drop('l', axis=1, inplace=True)
 
-# Split the data set up into a training and a test set. We can do whatever we want with the training set, but we may only use the test set once, to check the performance.
+# Discretize dataset
+est = KBinsDiscretizer(n_bins=4)
+est.fit(X)
+X = est.transform(X)
+
+y = df1['defects']
+
+# Split the data set up into a training and a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Preprocess the data, scaling features. For most regression methods this has no influence, but for some it does.
-if useScaler:
-    scaler = StandardScaler()  # Create a basic scaler object.
-    scaler.fit(X_train)  # Examine the data to find means and standard deviations.
-    X_train = scaler.transform(X_train)  # Normalize the data set.
-    X_train = pd.DataFrame(X_train,
-                           columns=X.columns)  # The scaler only returns the numbers, so we manually turn the numbers into a Pandas dataframe again, with the right column titles.
+# Fit the model
+model = ComplementNB()
+model.fit(X_train.toarray(), y_train)  # Train the model on the training data.
+y_pred = model.predict(X_test.toarray())
+print(f'Number of mislabeled points out of a total {X_test.shape[0]} points : {(y_test != y_pred).sum()}')
 
-# Set up a validation set to tune the parameters on.
-Xt, Xv, yt, yv = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
+# Calculate precision, recall and f1
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 
-# Set up our own evaluation function.
-def evaluateResults(title, model, Xt, Xv, yt, yv):
-    pred_train = model.predict(Xt)  # Predict values from the training set to determine the fit.
-    pred_val = model.predict(Xv)  # Predict values from the validation set to determine the fit.
-    RMSE_train = np.sqrt(mean_squared_error(yt, pred_train))
-    R2_train = r2_score(yt, pred_train)
-    RMSE_val = np.sqrt(mean_squared_error(yv, pred_val))
-    R2_val = r2_score(yv, pred_val)
-    print("The performance of {} on the training and validation set is:".format(title))
-    print("Training set: RMSE = {}, R2 = {}".format(RMSE_train, R2_train))
-    print("Validation set: RMSE = {}, R2 = {}\n".format(RMSE_val, R2_val))
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+f1 = (2 * precision * recall) / (precision + recall)
+print(f'precision: {precision}')
+print(f'recall: {recall}')
+print(f'f1: {f1}')
 
-
-# Try linear regression.
-model = LinearRegression()  # Set up a regression model.
-model.fit(Xt, yt)  # Train the model on the training data.
-evaluateResults('linear regression', model, Xt, Xv, yt, yv)
-
-print('fin')
+# Plot the precision recall curve
+plot_precision_recall_curve(model, X_test.toarray(), y_test)
